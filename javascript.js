fdea6c6af2a39dc51f61119e9f37fe6bf793a5ba
yiechernchuen <yccgithub@gmail.com>
2023-03-29 16:26:46 +0800

    Add button listener, update operate function

--- javascript.js
+++ javascript.js
@@ -12,16 +12,62 @@ function division(number1, number2) {
 }
 function operate() {
     if (operator === '+') {
-        addition(number1, number2);
+        displayOutputValue = addition(number1, number2);
     } else if (operator === '-') {
-        subtraction(number1, number2);
+        displayOutputValue = subtraction(number1, number2);
     } else if (operator === '*') {
-        multiplication(number1, number2);
+        displayOutputValue = multiplication(number1, number2);
     } else if (operator === '/') {
-        division(number1, number2);
+        displayOutputValue = division(number1, number2);
     }
+    display.textContent = displayOutputValue;
+    operatorClicked = false;
+    equalClicked = true;
+    number1 = displayOutputValue;
+    number2 = '';
+    operator = '';
 }
 
-let number1 = '2';
-let number2 = '2';
-let operator = '/';
+let number1 = '';
+let number2 = '';
+let operator = '';
+let displayOutputValue = '0';
+
+const display = document.querySelector('.display');
+const buttonDigits = document.querySelectorAll('.digits');
+const buttonOperator = document.querySelectorAll('.operator');
+const buttonDelete = document.querySelector('.delete');
+const buttonClear = document.querySelector('.clear');
+const buttonDecimal = document.querySelector('.decimal');
+const buttonEqual = document.querySelector('.equal');
+
+let operatorClicked = false;
+let equalClicked = false;
+
+buttonDigits.forEach((digit1) => {
+    digit1.addEventListener('click', (e) => {
+        if (!(operatorClicked || equalClicked)) {
+            number1 += e.target.innerText;
+            display.textContent = number1;
+        }
+    });
+});
+
+buttonDigits.forEach((digit2) => {
+    digit2.addEventListener('click', (e) => {
+        if (operatorClicked) {
+            number2 += e.target.innerText;
+            display.textContent = `${number1} ${operator} ${number2}`;
+        }
+    });
+});
+
+buttonOperator.forEach((buttonOperator) => {
+    buttonOperator.addEventListener('click', (e) => {
+        operatorClicked = true;
+        operator = e.target.innerText;
+        display.textContent = `${number1} ${operator}`;
+    });
+});
+
+buttonEqual.addEventListener('click', operate);
