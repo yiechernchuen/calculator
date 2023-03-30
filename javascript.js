870b688fc39df3f589308b623c6666160d45b0ee
yiechernchuen <yccgithub@gmail.com>
2023-03-31 01:39:50 +0800

    Add decimal function, amend code, improve code readability

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@ function operate() {
     } else if (operator === '*') {
         displayOutputValue = multiplication(number1, number2);
     } else if (operator === '/') {
-        if (!number2) {
+        if (!number2 || number2 === '0' || number2 === '0.') {
             clear();
             display2.textContent = 'ERROR';
             return;
@@ -32,6 +32,9 @@ function operate() {
     display2.textContent = displayOutputValue;
     operatorClicked = false;
     equalClicked = true;
+    decimalClicked = false;
+    number1HasDecimal = false;
+    number2HasDecimal = false;
     number1 = displayOutputValue;
     number2 = '';
     operator = '';
@@ -39,13 +42,20 @@ function operate() {
 function remove() {
     if (number1 && operator && number2) {
         number2 = number2.slice(0, -1);
-        if (!number2) number2 = '';
-        display2.textContent = '0';
+        number2HasDecimal = number2.includes('.');
+        display2.textContent = number2;
+        if (!number2 || number2 === '0') {
+            number2 = '';
+            display2.textContent = '0';
+        }
     } else if (number1 && !operator && !equalClicked) {
         number1 = number1.slice(0, -1);
+        number1HasDecimal = number1.includes('.');
         display2.textContent = number1;
-        if (!number1) number1 = '';
-        display2.textContent = '0';
+        if (!number1 || number1 === '0') {
+            number1 = '';
+            display2.textContent = '0';
+        }
     }
 }
 function clear() {
@@ -55,9 +65,43 @@ function clear() {
     displayOutputValue = '0';
     operatorClicked = false;
     equalClicked = false;
-    // decimalClicked = false;
+    decimalClicked = false;
+    number1HasDecimal = false;
+    number2HasDecimal = false;
     display1.textContent = '';
     display2.textContent = displayOutputValue;
+}
+
+function decimal() {
+    const number1Func = () => {
+        display2.textContent = number1;
+        number1HasDecimal = true;
+    };
+    const number2Func = () => {
+        display1.textContent = `${number1} ${operator}`;
+        display2.textContent = number2;
+        number2HasDecimal = true;
+    };
+    decimalClicked = true;
+    if (!number1 && !operatorClicked) {
+        number1 = '0.';
+        number1Func();
+    } else if (number1 && !operatorClicked && !number1HasDecimal) {
+        number1 += '.';
+        number1Func();
+    }
+    if (!number2 && operatorClicked) {
+        number2 = '0.';
+        number2Func();
+    } else if (number2 && operatorClicked && !number2HasDecimal) {
+        number2 += '.';
+        number2Func();
+    }
+    if (equalClicked) {
+        clear();
+        number1 = '0.';
+        number1Func();
+    }
 }
 
 let number1 = '';
@@ -78,30 +122,38 @@ display2.textContent = displayOutputValue;
 
 let operatorClicked = false;
 let equalClicked = false;
-// let decimalClicked = false;
+let decimalClicked = false;
+let number1HasDecimal = false;
+let number2HasDecimal = false;
 
 buttonDigits.forEach((digit1) => {
     digit1.addEventListener('click', (e) => {
         if (!(operatorClicked || equalClicked)) {
-            if (!number1 /*&& !decimalClicked*/) {
+            number1 += e.target.innerText;
+            if (number1 === '0') {
                 number1 = '';
+                display2.textContent = '0';
+            } else {
+                display2.textContent = number1;
             }
+        }
+        if (equalClicked && !number1HasDecimal) {
+            clear();
             number1 += e.target.innerText;
             display2.textContent = number1;
         }
-        if (equalClicked) {
-            clear();
-            equalClicked = false;
-            number1 = e.target.innerText;
-            display2.textContent = number1;
+        if (equalClicked && number1HasDecimal) {
+            number1 += e.target.innerText;
         }
     });
 });
 buttonOperator.forEach((buttonOperator) => {
     buttonOperator.addEventListener('click', (e) => {
         if (number1 && number2) operate();
+        if (display2.textContent === 'ERROR') return;
         if (number1 === '') number1 = '0';
         equalClicked = false;
+        decimalClicked = false;
         operatorClicked = true;
         operator = e.target.innerText;
         display1.textContent = `${number1} ${operator}`;
@@ -113,8 +165,13 @@ buttonDigits.forEach((digit2) => {
     digit2.addEventListener('click', (e) => {
         if (operatorClicked) {
             number2 += e.target.innerText;
-            display1.textContent = `${number1} ${operator}`;
-            display2.textContent = number2;
+            if (number2 === '0') {
+                number2 = '';
+                display2.textContent = '0';
+            } else {
+                display1.textContent = `${number1} ${operator}`;
+                display2.textContent = number2;
+            }
         }
     });
 });
@@ -122,6 +179,4 @@ buttonDigits.forEach((digit2) => {
 buttonEqual.addEventListener('click', operate);
 buttonDelete.addEventListener('click', remove);
 buttonClear.addEventListener('click', clear);
-
-// test click one digit, then divide then equal
-// start with nothing then click
+buttonDecimal.addEventListener('click', decimal);
